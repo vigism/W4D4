ad70e4956549140bc96de0d0d56c568031a4a72e
vigism <rushilvig@gmail.com>
2019-06-06 12:22:21 -0700

    Add user sign in/sign up

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if @user
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      @user = User.new
+      flash.now[:errors] = ["Invalid Credentials"]
+      render :new
+    end
+  end
+end
